3820023aaf35b4f57306ba0e9fbe96b4a77d6a9e
Shafiqul Islam <shafiqswh@gmail.com>
2024-02-08 14:45:09 -0800

    third commit

--- mail/contact.js
+++ mail/contact.js
@@ -15,8 +15,9 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "contact.php",
-                type: "POST",
+                //url: "contact.php",
+                url: "https://p7hyzxu66qwawwmvamgho62gdm0onmck.lambda-url.us-east-1.on.aws/",
+                type: "GET",
                 data: {
                     name: name,
                     email: email,
@@ -36,10 +37,12 @@ $(function () {
                 },
                 error: function () {
                     $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
-                    $('#success > .alert-danger').append('</div>');
+                    $('#success > .alert-danger').append($("<strong>").text("Your message has been sent"));
+                    //$('#success > .alert-success').append($("<strong>").text("Your message has been sent."));
+                    
+                    $('#success > .alert-success').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
                 complete: function () {
